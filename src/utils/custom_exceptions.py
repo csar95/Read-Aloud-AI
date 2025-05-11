07e3d9e34deb26728b0b9d0890922635216294e0
César Gutiérrez <cesarguti47@gmail.com>
2025-05-11 18:34:34 +0200

    Adds custom_exceptions.py

---
+++ src/utils/custom_exceptions.py
@@ -0,0 +1,11 @@
+class SystemException(Exception):
+    pass
+
+
+class OpenAIAPICallError(SystemException):
+    def __init__(self, openai_error: str, openai_error_message: str):
+        self.openai_error = openai_error
+        self.openai_error_message = openai_error_message
+        super().__init__(
+            f"OpenAI API call failed with error: {openai_error}: {openai_error_message}"
+        )
