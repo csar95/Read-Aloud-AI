6bd08a81cf0f1f037e044fc0533b731c4f66de80
César Gutiérrez <cesarguti47@gmail.com>
2025-05-15 18:42:07 +0200

    feat: Adds text extraction and formatting functionality & UI

---
+++ src/app.py
@@ -0,0 +1,73 @@
+from pathlib import Path
+import sys
+
+project_root = Path(__file__).resolve().parent.parent
+sys.path.append(str(project_root))
+
+import gradio as gr
+from huggingface_hub import hf_hub_download
+from kokoro import KModel
+
+from src.utils.constants import TTS_MODEL_REPO_ID
+from src.inference import generate_podcast_from_file
+
+
+demo = gr.Interface(
+    fn=generate_podcast_from_file,
+    inputs=[
+        gr.File(
+            file_count="single",
+            label="Upload a file",
+            file_types=[".pdf"],
+            type="binary",
+        ),
+        gr.Dropdown(
+            choices=["am_liam", "am_puck"],  # FIXME: TAKE CHOICES FROM KOKORO
+            value="am_liam",
+            label="Voice",
+            filterable=True,
+            multiselect=False,
+            allow_custom_value=False,
+        ),
+        gr.Slider(
+            minimum=0.25,
+            maximum=2,
+            step=0.25,
+            value=1,
+            label="Speed of speech",
+        ),
+        gr.Slider(
+            minimum=0,
+            maximum=1,
+            step=0.1,
+            value=0.3,
+            label="Duration of pauses in the speech (seconds)",
+        ),
+        gr.Textbox(label="Gemini API Key", type="text", lines=1, max_lines=None),
+        gr.Dropdown(
+            choices=["gemini-2.5-flash-preview-04-17", "gemini-2.5-pro-exp-03-25"],
+            value="gemini-2.5-flash-preview-04-17",
+            label="Gemini model ID",
+            filterable=True,
+            multiselect=False,
+            allow_custom_value=False,
+        ),
+    ],
+    outputs=[
+        gr.Audio(label="Podcast"),
+    ],
+)
+
+
+if __name__ == "__main__":
+    print("Downloading TTS model from Hugging Face Hub...")
+    tts_model_dir = Path("../models/kokoro")
+    tts_model_dir.mkdir(parents=True, exist_ok=True)
+    tts_model_path = hf_hub_download(
+        repo_id=TTS_MODEL_REPO_ID,
+        filename=KModel.MODEL_NAMES[TTS_MODEL_REPO_ID],
+        local_dir=tts_model_dir,
+        force_download=False,  # Set to True to force redownload even if the file exists
+    )
+
+    demo.launch(debug=True)
---
+++ src/inference.py
@@ -0,0 +1,234 @@
+from io import BytesIO
+import json
+from typing import List
+
+import gradio as gr
+import magic
+from openai import OpenAI
+from openai._exceptions import OpenAIError
+import pypdfium2 as pdfium
+
+from src.io_schemas.output_schemas import FormattedPageText
+from src.io_schemas.prompts import FORMAT_TEXT_FOR_TTS
+from src.openai_api_utils.controller import OpenAIAPIController
+from src.pdf_reader.helpers import detect_header_footer
+from src.utils.constants import (
+    DURATION_OF_ERROR_MESSAGE,
+    GEMINI_BASE_URL,
+    OPENAI_API_KWARGS,
+    SILENCE_KEYWORD,
+    SUPPORTED_FORMATS,
+)
+from src.utils.custom_exceptions import (
+    OpenAIInvalidResponseFormatError,
+    UnsupportedFileFormatError,
+)
+
+
+def setup_api_client(api_key: str, model_id: str) -> OpenAIAPIController:
+    """
+    Set up the OpenAI API client with the provided API key and initialize the controller
+    for the specified model ID.
+
+    Parameters
+    ----------
+    api_key : str
+        The API key for the OpenAI API.
+    model_id : str
+        The model ID to be used for inference.
+
+    Raises
+    ------
+    OpenAIError
+        If the API key is invalid or if there is an issue while setting up the client.
+
+    Returns
+    -------
+    OpenAIAPIController
+        An instance of the OpenAIAPIController initialized with the OpenAI client and
+        model ID.
+    """
+    if not api_key:
+        raise OpenAIError("API key is required.")
+    
+    try:
+        client = OpenAI(api_key=api_key, base_url=GEMINI_BASE_URL)
+        print(client.models.list().to_dict())
+    except Exception:
+        raise OpenAIError("Invalid API key or unable to connect to the OpenAI API.")
+
+    openai_api_ctrl = OpenAIAPIController(openai_client=client, model_name=model_id)
+    return openai_api_ctrl
+
+
+def get_file_format(file: BytesIO) -> str:
+    """
+    Get the file format of the given file.
+
+    Parameters
+    ----------
+    file: io.BytesIO
+        File to get the format from.
+
+    Returns
+    -------
+    str
+        File format.
+    """
+    try:
+        content = file.read()
+        mime = magic.Magic(mime=True)
+        file_type = mime.from_buffer(content)
+    finally:
+        file.seek(0)
+
+    return file_type
+
+
+def extract_text_from_pdf(pdf_file: BytesIO, pages: List[int] = None) -> List[str]:
+    """
+    Extract text from a PDF file, removing header and footer lines.
+
+    Parameters
+    ----------
+    pdf_file : io.BytesIO
+        The PDF file to extract text from.
+    pages : list of int, optional
+        List of page numbers (0-indexed) to extract text from. If None, all pages will
+        be processed. Default is None.
+
+    Returns
+    -------
+    list of str
+        List of strings, each representing the text extracted from a page of the PDF.
+    """
+
+    pdf = pdfium.PdfDocument(pdf_file)
+    print(f"Length of PDF: {len(pdf)} pages")
+
+    header_footer_lines = detect_header_footer(document=pdf)
+
+    text_from_pages = []
+    for page_id in pages or range(len(pdf)):
+        # It seems that the package "pypdfium2" separates lines by "\r\n" by default
+        page_text = pdf[page_id].get_textpage().get_text_bounded()
+
+        # Remove lines contained in header/footer
+        page_text_without_header_footer = "\n".join(
+            line
+            for line in page_text.splitlines()
+            if line.strip() not in header_footer_lines
+        )
+
+        text_from_pages.append(page_text_without_header_footer)
+
+    return text_from_pages
+
+
+def format_text_for_tts(
+    openai_api_controller: OpenAIAPIController, text_chunks: List[str]
+) -> str:
+    """
+    Format text chunks into clean, natural-language text suitable for Text-to-Speech
+    (TTS) using an LLM model.
+
+    Parameters
+    ----------
+    openai_api_controller : OpenAIAPIController
+        The OpenAI API controller to use for sending requests to the LLM model.
+    text_chunks : list of str
+        List of text chunks to format.
+
+    Raises
+    ------
+    OpenAIInvalidResponseFormatError
+        If the response from the OpenAI API does not match the expected format.
+
+    Returns
+    -------
+    str
+        The formatted text as a single string.
+    """
+    formatted_document_text = ""
+    for page_id, page_text in enumerate(text_chunks):
+        print(("-------------------------------------------------------------------"))
+        print(f"Processing page {page_id + 1}/{len(text_chunks)}")
+
+        # Get input texts that are needed to build the prompt
+        previous_fragment = (
+            f"... {formatted_document_text[-100:]}" if formatted_document_text else ""
+        )
+        current_page = page_text
+        next_preview = (
+            text_chunks[page_id + 1] if page_id + 1 < len(text_chunks) else ""
+        )
+
+        # Build the prompt object as the OpenAI API controller expects
+        prompt = {
+            "system_msg": FORMAT_TEXT_FOR_TTS.system_msg.format(
+                silence_keyword=SILENCE_KEYWORD,
+            ),
+            "user_msg": FORMAT_TEXT_FOR_TTS.user_msg.format(
+                previous_fragment=previous_fragment,
+                current_page=current_page,
+                next_preview=next_preview,
+            ),
+        }
+
+        # Send the request to the OpenAI/Gemini API
+        chat_completion, elapsed_time_s, retries_taken = (
+            openai_api_controller.send_request(
+                prompt=prompt,
+                response_format=FORMAT_TEXT_FOR_TTS.output_json,
+                **OPENAI_API_KWARGS,
+            )
+        )
+        num_attempts = retries_taken + 1
+        print(
+            f"Received response from OpenAI API. Response: {chat_completion}\n"
+            f"num_attempts: {num_attempts}\n"
+            f"response_time (s): {elapsed_time_s:.2f}"
+        )
+
+        # Validate response format. Raises custom exception if the response does not match the `FormattedPageText` schema
+        response_msg = chat_completion.choices[0].message.content
+        try:
+            formatted_page_text = FormattedPageText(**json.loads(response_msg))
+        except Exception as e:
+            raise OpenAIInvalidResponseFormatError()
+
+        formatted_document_text += f" {formatted_page_text.text}"
+
+    return formatted_document_text
+
+
+def generate_podcast_from_file(
+    file, voice, speed, duration_of_pauses, gemini_api_key, gemini_model_id
+):
+    try:
+        openai_api_ctrl = setup_api_client(
+            api_key=gemini_api_key, model_id=gemini_model_id
+        )
+
+        byte_stream = BytesIO(file)
+        file_format = get_file_format(file=byte_stream)
+        assert file_format in SUPPORTED_FORMATS, str(UnsupportedFileFormatError())
+
+        text_from_pages = extract_text_from_pdf(pdf_file=byte_stream, pages=list(range(4)))  # FIXME: REMOVE HARDCODED PAGES
+
+        formatted_text = format_text_for_tts(
+            openai_api_controller=openai_api_ctrl, text_chunks=text_from_pages
+        )
+
+        # TODO: PASS THE FORMATTED TEXT TO THE TTS MODEL
+
+    except OpenAIError as e:
+        print(f"ERROR: {e}")
+        raise gr.Error(
+            message="Gemini API client is not available. Please check your API key and model ID.",
+            duration=DURATION_OF_ERROR_MESSAGE,
+        )
+
+    except Exception as e:
+        print(f"ERROR: {e}")
+        raise gr.Error(message=str(e), duration=DURATION_OF_ERROR_MESSAGE)
